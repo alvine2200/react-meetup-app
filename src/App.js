716851a9e979d877ca40ed813e281675cd26eb58
alvine2200 <alvinellavu@gmail.com>
2022-09-21 17:47:06 +0300

    day 1

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route }  from 'react-router-dom';
 import AllMeetupsPage from './pages/Allmeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation/>
        <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupsPage />} />
